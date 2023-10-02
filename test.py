a119e969e79e8253630fa398a60e805dc6f51d6c
hyeonsu666 <138350307+hyeonsu666@users.noreply.github.com>
2023-10-02 23:54:53 +0900

    ttest

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
+import idautils
 import idc
 
-# 현재 선택한 함수의 시작 주소를 가져옴
 func_ea = idc.ScreenEA()
 
-# 함수 내의 모든 명령어를 순회하며 출력
+for func_ea in idautils.Functions():
+    func_name = idc.get_func_name(func_ea)
+    print(f"Function: {func_name}, Address: 0x{func_ea:X}")
+
 for head in idautils.FuncItems(func_ea):
     # 명령어 주소와 명령어 텍스트 출력
     print(f"Address: 0x{head:X}, Instruction: {idc.GetDisasm(head)}")
