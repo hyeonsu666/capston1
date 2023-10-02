22043026110ffb7a0df22b09315dedf6aeaa5f07
hyeonsu666 <138350307+hyeonsu666@users.noreply.github.com>
2023-10-03 00:08:44 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import idautils
 import idc
 
-func_ea = idc.ScreenEA()
+#func_ea = idc.ScreenEA()
 
 for func_ea in idautils.Functions():
     func_name = idc.get_func_name(func_ea)
     print(f"Function: {func_name}, Address: 0x{func_ea:X}")
 
-for head in idautils.FuncItems(func_ea):
+#for head in idautils.FuncItems(func_ea):
     # 명령어 주소와 명령어 텍스트 출력
-    print(f"Address: 0x{head:X}, Instruction: {idc.GetDisasm(head)}")
+    #print(f"Address: 0x{head:X}, Instruction: {idc.GetDisasm(head)}")
