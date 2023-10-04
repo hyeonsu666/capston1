5165afeb06a09c8464981e6c3ab86cc4bfc38dd7
limit59 <qkrwogks59@gmail.com>
2023-10-04 09:08:16 +0900

    Update 명령어 변경.py

--- 명령어 변경.py
+++ 명령어 변경.py
@@ -1,38 +1,40 @@
-import idc
-import idautils
-import idaapi
-
-def obfuscate_functions():
-    for func_ea in idautils.Functions():
-        func = idaapi.get_func(func_ea)
-        if func is not None:
-            obfuscate_function(func)
-
-# 함수 내용 난독화
-def obfuscate_function(func):
-    for block in idaapi.FlowChart(func):
-        for head in idautils.Heads(block.startEA, block.endEA):
-            if is_instruction_to_obfuscate(head):
-                obfuscate_instruction(head)
-                print(f"Obfuscated instruction at 0x{head:X}")
-
 # 난독화할 대상 명령 확인
-def is_instruction_to_obfuscate(ea):
-    mnem = idc.GetMnem(ea)
-    return mnem in ["add", "sub", "xor", "mov"]
+def is_instruction_to_obfuscate(mnemonic):
+    return mnemonic in ["add", "sub", "xor", "mov"]
 
 # 명령 난독화
-def obfuscate_instruction(ea):
-    operand_count = idc.GetOpType(ea, 0)
-    if operand_count >= 2:
-        operand1 = idc.GetOperandValue(ea, 0)
-        operand2 = idc.GetOperandValue(ea, 1)
+def obfuscate_instruction(instruction):
+    if len(instruction) >= 2:
+        operand1 = instruction[0]
+        operand2 = instruction[1]
         new_operand1 = operand1 + operand2  # 예: 두 피연산자의 합
-        idc.OpSetOperandType(ea, 0, idc.o_imm)
-        idc.OpSetOperandValue(ea, 0, new_operand1)
+        return f"mov {operand1}, {new_operand1}"  # 난독화된 명령 반환
+    return None
+
+# 난독화할 함수 내용
+def obfuscate_function(function):
+    obfuscated_code = []
+    for instruction in function:
+        mnemonic = instruction[0]
+        if is_instruction_to_obfuscate(mnemonic):
+            obfuscated = obfuscate_instruction(instruction)
+            if obfuscated:
+                obfuscated_code.append(obfuscated)
+        else:
+            obfuscated_code.append(' '.join(instruction))
+    return obfuscated_code
 
 # 스크립트 실행
 if __name__ == "__main__":
-    obfuscate_functions()
+    # 테스트용 함수 내용
+    sample_function = [
+        ["mov", "eax", "10"],
+        ["add", "eax", "20"],
+        ["mov", "ebx", "30"],
+        ["sub", "ebx", "5"],
+    ]
 
-#명령어 변경ㄴㄴㄴㄴ
+    obfuscated_code = obfuscate_function(sample_function)
+
+    for line in obfuscated_code:
+        print(line)
